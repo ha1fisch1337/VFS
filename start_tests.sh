f3a703a16f767c5f0cccc5e56640763c19fda561
ha1fisch1337 <89041784+ha1fisch1337@users.noreply.github.com>
2025-10-11 11:07:33 +0300

    Автоматизированное тестирование

---
+++ start_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python main.py -p smth.xml -s test4s.txt
+python main.py -p smth.xml -s testrm.txt
+python main.py -p smth.xml -s testvfs.txt
+
+python main.py -p smth1.xml -s test4s.txt
+python main.py -p smth1.xml -s testrm.txt
+python main.py -p smth1.xml -s testvfs.txt
+
+python main.py -p smth2.xml -s test4s.txt
+python main.py -p smth2.xml -s testrm.txt
+python main.py -p smth2.xml -s testvfs.txt
